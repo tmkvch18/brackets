0f6ff598b611bed8594f5beb2ea3a4f9681ba1d2
tmkvch18 <tmkvch18@gmail.com>
2024-10-28 00:02:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+
+  if (str.length % 2) {
+    return false;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      const open = bracketsConfig[j][0];
+      const close = bracketsConfig[j][1];
+
+      if (current === close) {
+        if (stack[stack.length - 1] === open && stack.length) {
+          stack.pop();
+        } else if (close === open) {
+          stack.push(current);
+        } else {
+          return false;
+        }
+      } else if (current === open) {
+        stack.push(current);
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
